954d63b73b6340e073e8461e42cb96d8b341a633
Hannael-mich <83248006+Hannael-mich@users.noreply.github.com>
2022-05-11 22:11:24 -0500

    Update script.js

--- HU-06/script.js
+++ HU-06/script.js
@@ -1,57 +1,83 @@
-const CARA = 0;
-        const CRUZ = 1;
+const CARA = "cara";
+const CRUZ = "cruz";
 
-        const ganar = 0;
-        const perder = 1;
+const ganar = 0;
+const perder = 1;
 
-        const caraBtn = document.getElementById("cara");
-        const cruzBtn = document.getElementById("cruz");
+const caraBtn = document.getElementById("cara");
+const cruzBtn = document.getElementById("cruz");
+const resultText = document.getElementById("start-text");
+const userImge = document.getElementById("user-img")
+const machineImg = document.getElementById("machine-img");
 
-        caraBtn.addEventListener("click",()=>{
-            play(CARA);
-        });
-        cruzBtn.addEventListener("click",()=>{
-            play(CRUZ);
-        });
+caraBtn.addEventListener("click",()=>{
+    play(CARA);
+});
 
-        function play(userOption){
-            const machineOption = Math.floor(Math.random()*2);
-            const result = calcResult(userOption, machineOption);
+cruzBtn.addEventListener("click",()=>{
+   play(CRUZ);
+});
 
-            switch(result){
-                case ganar:
-                    alert(machineOption + "ganaste");
-                    break;
-                case perder:
-                    alert(machineOption + "perdiste");
-                    break;
-            }
+function play(userOption){
+    const machineOption = calcMachineOption();
+    const result = calcResult(userOption, machineOption);
+    
+
+
+    machineImg.src =  eleccion();
+
+    function eleccion(){
+        if(machineOption == CARA){
+            return "https://m.media-amazon.com/images/I/51kEkau11GL._AC_SX450_.jpg";
+        }else{
+            return "https://img.freepik.com/vector-gratis/moneda-oro-1609-segovia-silueta-hecha-mano-cortada-laser_589800-67.jpg?w=740";
         }
+    }
+    
 
-        function calcResult(userOption, machineOption){
-            if(userOption === machineOption){
-                return ganar;
-            }else{
-                return perder;
-            }
-        }
+    switch(result){
+        case ganar:
+            resultText.innerHTML = "¡GANASTE!";
+            break;
+        case perder:
+            resultText.innerHTML = "¡PERDISTE!"
+             break;
+    }
+}
+function calcMachineOption(){
+    const number = Math.floor(Math.random()*2);
+    switch(number){
+        case 0:
+            return CARA;
+        case 1:
+            return CRUZ;
+    }
+    
+}
+function calcResult(userOption, machineOption){
+    if(userOption === machineOption){
+        return ganar;
+    }else{
+        return perder;
+    }
+}
         
-        function obtenerJuegos(){
+function obtenerJuegos(){
             
-            let peticion = new XMLHttpRequest();
+    let peticion = new XMLHttpRequest();
             
-            peticion.onreadystatechange = procesarJuegos;
+    peticion.onreadystatechange = procesarJuegos;
 
-            peticion.open("GET","https://califas.mocklab.io/v1/juegos", true);
+    peticion.open("GET","https://califas.mocklab.io/v1/juegos", true);
             
-            peticion.send();
+    peticion.send();
 
-        }
-        function procesarJuegos(){
+}
+function procesarJuegos(){
             
-            console.log(this.status);
+console.log(this.status);
 
-            let respuesta = JSON.parse(this.responseText);
+    let respuesta = JSON.parse(this.responseText);
 
-            console.log(respuesta);
-        }
+    console.log(respuesta);
+}
