dc652d24fe852420f89f5ed515b1022b12ee3c42
lividineitor <lividineitor@gmail.com>
2022-05-19 21:39:24 -0500

    Funcionalidad de detección de ganador

--- v0.3/js/scriptPpt.js
+++ v0.3/js/scriptPpt.js
@@ -5,6 +5,10 @@ const recurso = 'ppts' ;
 
 var usuarioId ;
 
+let pptId ;
+
+let refTimer ;
+
 let PptDto = {
 
     pptId: 0 ,
@@ -48,7 +52,9 @@ const machineImg = document.getElementById("machine-img");
 
 function juego(userOption){
 
-    let pptId = PptDto.pptId ;
+    pptId = PptDto.pptId ;
+
+    refTimer = setInterval ( obtenerResultados , 1000 ) ;
 
     let ruta = backend + recurso + '/' + pptId + "?usuarioId=" + this.usuarioId + "&action=eleccion&eleccion=" + userOption ;
 
@@ -61,196 +67,68 @@ function juego(userOption){
 
     peticion.send();
 
-}
-
-function obtenerResultados () {
-
-    let pptId = PptDto.pptId ;
-
-    let ruta = backend + recurso + '/' + pptId ;
-
-    let peticion = new XMLHttpRequest();
-
-    peticion.onreadystatechange = procesarJuegos;
-
-    peticion.open("GET", ruta , true);
-
-    peticion.setRequestHeader ( "Content-Type" , "application/json" ) ;
-    peticion.setRequestHeader ( "Accept" , "application/json" ) ;
-
-
-    peticion.send();
+    
 
 }
-
-/*function play(userOption) {
-    if(isPlaying) return;
-
-    isPlaying = true;
-
-    userImg.src = "img/" + userOption + ".svg";
-
-    resultText.innerHTML = "Escojiendo";
-
-    const interval = setInterval(function(){
-        const machineOption = calcMachineOption();
-        machineImg.src = "img/" + machineOption + ".svg";
-    }, 200);
-
-    setTimeout(function () {
-
-        clearInterval(interval);
-
-        const machineOption = calcMachineOption();
-        const result = calcResult(userOption, machineOption);
-
-        machineImg.src = "img/" + machineOption + ".svg";
-
-        switch (result) {
-            case TIE:
-                resultText.innerHTML = "Empate";
-                break;
-            case WIN:
-                resultText.innerHTML = "¡Ganaste!";
-                break;
-            case LOST:
-                resultText.innerHTML = "¡Perdiste!";
-                break;
-        }
-        isPlaying = false;
-    }, 2000);
-}
-
-function calcMachineOption() {
-    const number = Math.floor(Math.random() * 3);
-    switch (number) {
-        case 0:
-            return ROCK;
-        case 1:
-            return PAPER;
-        case 2:
-            return SCISSORS;
-    }
-}
-//Codigo que es posible quitar
-function calcResult(userOption, machineOption) {
-    if (userOption === machineOption) {
-        return TIE;
-
-    } else if (userOption === ROCK) {
-
-        if (machineOption === PAPER) return LOST;
-        if (machineOption === SCISSORS) return WIN;
-
-    } else if (userOption === PAPER) {
-
-        if (machineOption === SCISSORS) return LOST;
-        if (machineOption === ROCK) return WIN;
-
-    } else if (userOption === SCISSORS) {
-
-        if (machineOption === ROCK) return LOST;
-        if (machineOption === PAPER) return WIN;
-
-    }
-}
-
-//Hasta aqui se tiene que quitar
-
-function cargaTodo () {
-
-    obtenerJuegoPPT () ;
-
-
-}*/
-
-
-function procesarJuegos(){
-    if(this.status==200){
-
-        let backPpt = JSON.parse ( this.responseText ) ;
-
-        let election ;
-
-        if ( backPpt.usuarioId1 == usuarioId )
-            election = backPpt.eleccion1 ;
-
-        else
-            election = backPpt.eleccion2 ;
-
-        document.getElementById('user-img').setAttribute ( "src" , "./images/" + election + ".svg" ) ;
-
-        resultText.innerHTML = "Escojiendo";
-
-        if ( backPpt.ganador == -1 )
-            resultText.innerHTML = "Empate";
-
-        else if ( backPpt.ganador == usuarioId )
-                resultText.innerHTML = "¡Ganaste!";
-
-        else
-                resultText.innerHTML = "¡Perdiste!";
-
-        console.log ( backPpt.ganador ) ;
-                console.log ( usuarioId ) ;
 
 
 /*
+ *
+ * Función que inicia un temporizador para obtener el resultado del juego
+ *
+ */
+function obtenerResultados () {
 
-    const interval = setInterval(function(){
-        machineImg.src = "../images/" + PptDto. + ".svg";
-    }, 200);
+    let lanzador = new XMLHttpRequest () ;
 
-    setTimeout(function () {
+    lanzador.onreadystatechange = terminaJuego ;
 
-        clearInterval(interval);
+    lanzador.open ( 'GET' , backend + recurso + '/' + pptId , true) ;
 
-        const machineOption = calcMachineOption();
-        const result = calcResult(userOption, machineOption);
+    lanzador.setRequestHeader ( 'Content-Type' , 'application/json' ) ;
+    lanzador.setRequestHeader ( 'Accept' , 'application/json' ) ;
 
-        machineImg.src = "../images/" + machineOption + ".svg";
-
-        switch (result) {
-            case TIE:
-                resultText.innerHTML = "Empate";
-                break;
-            case WIN:
-                resultText.innerHTML = "¡Ganaste!";
-                break;
-            case LOST:
-                resultText.innerHTML = "¡Perdiste!";
-                break;
-        }
-        isPlaying = false;
-    } , 2000 ) ;
-    }else{
-        console.log(this.status);*/
-    }
-
-
+    lanzador.send () ;
 
 }
 
-/*function gestorRespuestaPpt () {
 
-    console.log("paso por aqui");
-    //if ( this.readyState == 4 ) {
+/*
+ *
+ * Función que al encontrar el resultado detiene el temporizador
+ *
+ */
+function terminaJuego () {
 
-        switch (this.status){
-            case 204:
-                console.log("La petición se ha completado con éxito pero su respuesta no tiene ningún contenido");
-                break;
-            case 404:
-                console.log("Algun tipo de error");
-                break;
-            case 503:
-                console.log("El servidor no está listo para manejar la petición.");
-                break;
-            default:
-                console.log("Error desconocido");
-                break;
+    if ( this.readyState == 4 ) {
+
+        let cuerpo = JSON.parse ( this.responseText ) ;
+
+        if ( cuerpo.ganador !== 0 ) {
+
+            clearInterval ( refTimer ) ;
+
+            let election ;
+
+            if ( cuerpo.usuarioId1 == usuarioId )
+                election = cuerpo.eleccion1 ;
+
+            else
+                election = cuerpo.eleccion2 ;
+
+            document.getElementById('user-img').setAttribute ( "src" , "./images/" + election + ".svg" ) ;
+
+            resultText.innerHTML = "Escojiendo";
+
+            if ( cuerpo.ganador == -1 )
+                resultText.innerHTML = "Empate";
+
+            else if ( cuerpo.ganador == usuarioId )
+                    resultText.innerHTML = "¡Ganaste!";
+
+            else
+                    resultText.innerHTML = "¡Perdiste!";
+
         }
-        console.log("Este es el estado " + this.status);
-    //}
-};*/
-
+    }
+}
